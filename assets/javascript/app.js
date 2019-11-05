b1685fc485ef070786da1d169f15cc0224d1304a
Hyejin Kim <cshjkin@gmail.com>
2019-11-04 23:00:59 -0800

    thinking how to run the trivia games getting questions & answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,14 +10,11 @@ $(document).ready(function() {
     var losses = 0;
 
     //  Interval Demonstration
-    //  Set our number counter to 100.
     var number = 30;
 
-    //  Variable that will hold our interval ID when we execute
     //  the "run" function
-    
     var intervalId;
-    //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
+   
     function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
@@ -26,38 +23,27 @@ $(document).ready(function() {
 
     //  The decrement function.
     function decrement() {
-
-    //  Decrease number by one.
-    number--;
+    number--;}
   
     //  Show the number in the #show-number tag.
     $("#show-number").html("<h2>" + number + "</h2>");
 
     //  Once number hits zero...
     if (number === 0) {
-
-        //  ...run the stop function.
         stop();
-
         }
-    }
 
     //  The stop function
     function stop() {
-
-    //  Clears our intervalId
-    //  We just pass the name of the interval
-    //  to the clearInterval function.
     clearInterval(intervalId);
     }
-
-    //  Execute the run function.
     run();
-
+    
+    // function for reset
     function reset() {
 
     }
-
+    //?? this better or should do in other way...? Q & A?...
     function won() {
         wins++;
         $("#numberWins").text(wins);
@@ -69,5 +55,34 @@ $(document).ready(function() {
         $("#numberLosses").text(losses);
         reset();
     }
+
+      // questions options and answers data
+  questions: {
+    q1: 'Which actress has won the most Oscars?',
+    q2: 'Which chess piece can only move diagonally?',
+    q3: 'How many valves does a trumpet have?',
+    q4: 'When did the Cold War end?',
+    q5: "In the Marvel cinematic universe, what is the name of Thor's home planet?",
+    q6: 'What does the "E" stand for in the name of the Chuck E Cheese?',
+    q7: "What is the only Major League Baseball team to never make it to the World Series?"
+  },
+  options: {
+    q1: ['Audley Hepburn', 'Jessica Simpson', 'Meryl Streep', 'Katherine Hepburn'],
+    q2: ['Queen', 'Bishop', 'Knight', 'Pawn'],
+    q3: ['5', '2', '1', '3'],
+    q4: ['1989', '1991', '1985', '1986'],
+    q5: ['Darbia','Asgard','Earth','Vormir'],
+    q6: ['Edgar','Edward','Entertainment','Energy'],
+    q7: ['Seattle Mariners', 'LA Dodgers', 'Washington National','Houston Astros']
+  },
+  answers: {
+    q1: 'Katherine Hepburn',
+    q2: 'Bishop',
+    q3: '3',
+    q4: '1989',
+    q5: 'Asgard',
+    q6: 'Entertainment',
+    q7: 'Seattle Mariners'
+  },
 
 });
