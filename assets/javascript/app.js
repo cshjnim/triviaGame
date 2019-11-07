bc0b26b4c604b755ebd59393c17d6df31fe6bc57
Hyejin Kim <cshjkin@gmail.com>
2019-11-06 19:34:37 -0800

    check for debugging and update the javascript and update button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,19 +2,20 @@ console.log("See this to try if this works")
 
 // jquery for game playing
 
-$(document).ready (function (){
-  $("remaining-time").hide();
+$(document).ready(function() {
+  $("#remaining-time").hide();
   $("#start").on ("click", game.startGame);
-  $(document).on("click", '.option',game.guessChecker);
+  $(document).on("click", '.option', game.guessChecker);
 })
   
 var game = {
   correct: 0,
   incorrect: 0,
   unanswered: 0,
-  timer: 30,
+  timer: 20,
   timerOn: false,
   timerId: '',
+  currentSet: 0,
 
   // questions options and answers data
  questions: {
@@ -33,7 +34,7 @@ options: {
   q4: ['1989', '1991', '1985', '1986'],
   q5: ['Darbia','Asgard','Earth','Vormir'],
   q6: ['Edgar','Edward','Entertainment','Energy'],
-  q7: ['Seattle Mariners', 'LA Dodgers', 'Washington National','Houston Astros']
+  q7: ['Seattle Mariners', 'LA Dodgers', 'DC National','Houston Astros']
 },
 answers: {
   q1: 'Katherine Hepburn',
@@ -45,88 +46,103 @@ answers: {
   q7: 'Seattle Mariners'
 },
   
-  startGame : function() {
+  startGame : function(){
     //variable
-    trivia.currentSet = 0;
-    trivia.correct =0;
-    trivia.incorrect=0;
-    trivia.unanswered = 0;
-    clearInterval(trivia.timerId);
+    game.currentSet = 0;
+    game.correct =0;
+    game.incorrect=0;
+    game.unanswered = 0;
+    clearInterval(game.timerId);
     //how the page should display
     $('#game').show();
     $('#results').html('');
-    $('#timer').text(trivia.timer);
+    $('#timer').text(game.timer);
     $('#start').hide();
     $('#remaining-time').show();
 
-    trivia.nextQuestion ();
+    game.nextQuestion ();
   },
-  
+
+  // how to run next questions
   nextQuestion: function(){
-    trivia.timer =30;
+    game.timer =20;
     $('#timer').removeClass('last-seconds');
-    $('#timer').text(trivia.timer);
-    if(!trivia.timerOn){
-      trivia.timerId = setInterval(trivia.timerRunning, 1000);
+    $('#timer').text(game.timer);
+    if(!game.timerOn){
+      game.timerId = setInterval(game.timerRunning, 1000);
     }
     
-    var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+    var questionContent = Object.values(game.questions)[game.currentSet];
     $('#question').text(questionContent);
-    var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+    var questionOptions = Object.values(game.options)[game.currentSet];
     $.each(questionOptions, function(index, key){
       $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
     })
   },
 
-  guessChecker: function(){
+// when timer runs out
+  timerRunning: function (){
+    if(game.timer > -1 && game.currentSet < Object.keys(game.questions).length){
+      $('#timer').text(game.timer);
+      game.timer--;
+        if(game.timer === 4){
+          $('#timer').addClass('last-seconds');
+        }
+      }
+        else if(game.timer === -1){
+          game.unanswered++;
+          game.result = false;
+          clearInterval(game.timerId);
+          resultId = setTimeout(game.guessResult, 1000);
+          $('#results').html('<h3>Out of time! The answer was '+ Object.values(game.answers)[game.currentSet] +'</h3>');
+        }
+
+    else if(game.currentSet === Object.keys(game.questions).length){
+    
+      // adds results here
+      $('#results')
+        .html('<h3>Play again!</h3>'+
+        '<p>Correct: '+ game.correct +'</p>'+
+         '<p>Incorrect: '+ game.incorrect +'</p>'+
+        '<p>Unaswered: '+ game.unanswered +'</p>');
+      
+      $('#game').hide();
+      $('#start').show();}
+    },
+
+    // method to evaluate the option clicked
+  guessChecker : function(){
+
     var resultId;
-    var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+    var currentAnswer = Object.values(game.answers)[game.currentSet];
     //if correct
     if ($(this).text() === currentAnswer) {
       $(this).addClass('btn-success').removeClass('btn-info');
-      trivia.correct++;
-      clearInterval(trivia.timerId);
-      resultId = setTimeout(trivia.guessResult, 1000);
+      game.correct++;
+      clearInterval(game.timerId);
+      resultId = setTimeout(game.guessResult, 1000);
       $('#results').html('<h3>Correct!</h3>');
     }
     //if wrong
     else {
       $(this).addClass('btn-danger').removeClass('btn-info');
     
-      trivia.incorrect++;
-      clearInterval(trivia.timerId);
-      resultId = setTimeout(trivia.guessResult, 1000);
+      game.incorrect++;
+      clearInterval(game.timerId);
+      resultId = setTimeout(game.guessResult, 1000);
       $('#results').html('<h3>Wrong! Answer is: '+ currentAnswer +'</h3>');
     }
   },
-
-  timerRunning: function (){
-    if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.qustions).length){
-      $('#timer').text(trivia.timer);
-      trivia.timer--;
-        if(trivia.timer === 4){
-          $('#timer').addClass('last-seconds');
-        }
-        else if(trivia.timer === -1){
-          trivia.unanswered++;
-          trivia.result = false;
-          clearInterval(trivia.timerId);
-          resultId = setTimeout(trivia.guessResult, 1000);
-          $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
-        }
-
-    else if(trivia.currentSet === Object.keys(trivia.questions).length){
+  // method to remove previous question results and options
+  guessResult : function(){
     
-      // adds results here
-      $('#results')
-        .html('<h3>Play again!</h3>'+
-        '<p>Correct: '+ trivia.correct +'</p>'+
-        '<p>Incorrect: '+ trivia.incorrect +'</p>'+
-        '<p>Unaswered: '+ trivia.unanswered +'</p>');
-      
-      $('#game').hide();
-      $('#start').show();
+    game.currentSet++;
+    
+    $('.option').remove();
+    $('#results h3').remove();
+
+    game.nextQuestion();
+    
 
   }
 }
-}}
